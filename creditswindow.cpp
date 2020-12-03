d27f51fcf00cc4c43308b21fca8d52b9c92052ab
Spencer Poultney <spoultne@uwo.ca>
2020-12-02 19:24:44 -0500

    Update creditswindow.cpp
    add comments

--- creditswindow.cpp
+++ creditswindow.cpp
@@ -1,11 +1,17 @@
-/**Credits Window class UI designed by Mazen Baioumy
- * This class was designed to display the names of group members who
- * contributed to this project.
- **/
+/** Credits window class
+This class was designed to display the names of group members who
+ contributed to this project.
+ @author Mazen Baioumy
+ @brief Displays form containing group members
+*/
 
 #include "creditswindow.h"
 #include "ui_creditswindow.h"
 
+/**
+ * @brief creates credits window
+ * @param parent
+ */
 creditsWindow::creditsWindow(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::creditsWindow)
@@ -13,6 +19,9 @@ creditsWindow::creditsWindow(QWidget *parent) :
     ui->setupUi(this);
 }
 
+/**
+ * @brief deletes credits window
+ */
 creditsWindow::~creditsWindow()
 {
     delete ui;
