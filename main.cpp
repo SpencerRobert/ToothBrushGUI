dc6649093d0eabaa7632780cb295e76f35953883
Spencer Poultney <spoultne@uwo.ca>
2020-12-02 19:25:45 -0500

    Update main.cpp
    add comments

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
-/**
+/** Main class
  * This class is designed to launch the start window which acts as the
  * opening interace of our program.
- **/
+ * @author Obaida Kamil
+ * @brief launches start window
+ */
 
 #include "startwindow.h"
 #include <QApplication>
 
+/**
+ * @brief main
+ * @param argc
+ * @param argv
+ * @return exit status
+ */
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
